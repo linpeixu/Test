b333990fb10dca85777fafb45892fab8400598c7
linpeixu <1966353889@qq.com>
2018-08-16 15:11:31 +0800

    Revert "99"
    This reverts commit 67291b34724b3432a0ce182aeb6c385914a85e10.

--- app/src/main/java/com/example/lpx/gitcommand/MainActivity.java
+++ app/src/main/java/com/example/lpx/gitcommand/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
     }
 
     private void init() {
@@ -18,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void test() {
-//增加的内容
+
     }
 
 
